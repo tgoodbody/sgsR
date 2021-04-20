1e48398bbc76dd1a8ff97e2a4261f9ecd6235bd9
Tristan Goodbody <60200460+tgoodbody@users.noreply.github.com>
2021-04-20 12:15:47 -0700

    vignette

--- vig/vignette.R
+++ vig/vignette.R
@@ -13,25 +13,37 @@ poly_subset <- st_union(poly_subset)
 poly_subset <- vect(poly_subset)
 #--- mask input ALS raster using polygon layer ---#
 
-wall_poly <- terra::mask(raster,poly_subset)
+wall_poly <- terra::mask(raster,
+                         poly_subset)
 
 #--- import access layer to be used during sampling if desired ---#
 
 roads <- st_read("C:/Users/goodb/Documents/UBC/post_doc/sgsR/vig/data/roads.shp")
 
 #--- perform stratification using k-means ---#
-kmeans <- strat_kmeans(raster = wall_poly[[5]], k = 4) ### note some values of k dont seem to give the right output
+kmeans <- strat_kmeans(raster = wall_poly[[5]], 
+                       k = 4) ### note some values of k dont seem to give the right output
 ## maybe this is related to the raster layers?
 
 #--- perform stratification using OSB ---#
 #--- note that this one can take a while ---#
-osb <- strat_osb(raster = wall_poly, metric = "wal_5", h = 4, n = 100) # should integrate functionality to do 2 metrics concurrently and create unique strata
+osb <- strat_osb(raster = wall_poly, 
+                 metric = "wal_5", 
+                 h = 4, 
+                 n = 100) # should integrate functionality to do 2 metrics concurrently and create unique strata
 
 #--- perform stratification using principal components ---#
-pcomp <- strat_pcomp(raster = wall_poly, ncp = 2, b1 = 4, b2 = 3) # should integrate functionality to allow users to define strata based on more than just PC1 and PC2
+pcomp <- strat_pcomp(raster = wall_poly, 
+                     ncp = 2, 
+                     b1 = 4, 
+                     b2 = 3) # should integrate functionality to allow users to define strata based on more than just PC1 and PC2
 
 #--- perform stratification using individual metrics ---#
-metrics <- strat_metrics(raster = wall_poly, metric = "wal_5", metric2 = "wal_2", b = 10, b2 = 5)
+metrics <- strat_metrics(raster = wall_poly, 
+                         metric = "wal_5", 
+                         metric2 = "wal_2", 
+                         b = 10, 
+                         b2 = 5)
 
 #--- define desired stratification raster ---#
 raster <- kmeans$raster
@@ -67,7 +79,8 @@ strat_w <- sample_strat(raster = raster,
 #--- extract strata from raster for already existing sample network ---#
 #--- we use random samples defined above ---#
 
-existing <- extract_existing(raster,srs_wo)
+existing <- extract_existing(raster,
+                             srs_wo)
 
 #--- sampling **with** access defined **and** existing samples defined ---#
 
@@ -85,6 +98,7 @@ strat_w_e <- sample_strat(raster = raster,
 
 #--- extract metrics from multi-band ALS raster for potential modeling ---#
 
-metrics <- extract_metrics(wall_poly,strat_w_e$samples)
+metrics <- extract_metrics(wall_poly,
+                           strat_w_e$samples)
 
 
