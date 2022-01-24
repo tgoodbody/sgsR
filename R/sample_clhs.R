23bcba97f95cf0d49e0dc70fc42a68ee29f00533
Tristan Goodbody <60200460+tgoodbody@users.noreply.github.com>
2022-01-24 12:18:04 -0800

    Update sample_clhs.R
    rem  xy function

--- R/sample_clhs.R
+++ R/sample_clhs.R
@@ -353,14 +353,3 @@ sample_clhs <- function(mraster,
     return(samples)
   }
 }
-
-# https://github.com/r-spatial/sf/issues/231#issuecomment-290817623
-sfc_as_cols <- function(x, names = c("X","Y")) {
-  stopifnot(inherits(x,"sf") && inherits(sf::st_geometry(x),"sfc_POINT"))
-  ret <- sf::st_coordinates(x)
-  ret <- tibble::as_tibble(ret)
-  stopifnot(length(names) == ncol(ret))
-  x <- x[ , !names(x) %in% names]
-  ret <- setNames(ret,names)
-  dplyr::bind_cols(x,ret)
-}
